62174439c63a1e9e02b8e8140843bd377e187855
Zhu <406132631@qq.com>
2019-09-01 23:05:23 +0800

    main process renderer process ipc通信

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -1,53 +1,32 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
+// app.on监听一个ready方法 也就是窗口已经准备完毕 可以被渲染了 执行对应的创建窗口的函数
+app.on('ready', () => {
+  // 通过BrowserWindow模块来新建一个窗口 
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    // 是否允许使用node api
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true
     }
   })
-
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+  // const secondWindow = new BrowserWindow({
+  //   width: 300,
+  //   height: 400,
+  //   // 指定父窗口 如果父窗口关闭了子窗口跟着关闭
+  //   parent: mainWindow,
+  //   // 是否允许使用node api
+  //   webPreferences: {
+  //     nodeIntegration: true
+  //   }
+  // })
+  // secondWindow.loadFile('second.html')
+  
+  // 主进程根据事件名称进行监听 并且可以利用event对象下的sender发送信息给renderer process
+  ipcMain.on('message', (event, arg) => {
+    console.log(arg)
+    event.sender.send('replay', 'replay from main')
   })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- electron-quick-start/renderer.js
+++ electron-quick-start/renderer.js
@@ -1,3 +1,10 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// 利用ipcRenderer进行进程通信
+const { ipcRenderer } = require('electron')
+// 监听页面Dom渲染完成以后 利用Electron提供的ipcRender来发送一个事件两个参数 一个是事件名称(message) 一个是携带的参数(String)
+window.addEventListener('DOMContentLoaded', () => {
+  ipcRenderer.send('message', 'hello from render')
+  ipcRenderer.on('replay', (event, arg) => {
+    console.log(arg)
+    document.getElementById('message').innerHTML = arg
+  })
+})
